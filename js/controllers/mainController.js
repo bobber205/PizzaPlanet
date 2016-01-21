f78fc294b646e81421d3b77bd5559558379e1fd1
Jing Wait <jingzhaowait@gmail.com>
2016-01-20 23:10:15 -0800

    add add edit delete menu

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,20 +1,53 @@
 app.controller('mainController', ["$scope",function($scope) {
   $scope.today = new Date();
+  $scope.addAppetizer = function() {
+    $scope.addingNewApp = false;
+    newappetizer = {
+      price: $scope.newPrice,
+      description: $scope.newDescription,
+      name: $scope.newName,
+      deleted: false
+    };
+    $scope.appetizers.unshift(newappetizer);
+    $scope.newName = $scope.newPrice = $scope.newDescription = "";
+  };
+  $scope.cancelAddAppetizer = function () {
+    $scope.addingNewApp = false;
+    $scope.newName = $scope.newPrice = $scope.newDescription = "";
+  };
+  
+  $scope.completeEditApp = function() {
+    $scope.editApp = false;
+     
+  };
+   
+  $scope.deleteEditApp = function() {
+    $scope.editApp = false;
+     
+  };
+  $scope.cancelEditApp = function() {
+    $scope.editApp = false;
+     
+  };
+  
   $scope.appetizers = [
     {
       name: 'Caprese',
       description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
-      price: 4.95
+      price: 4.95,
+      deleted: false
     },
     {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
-      price: 3.95
+      price: 3.95,
+      deleted: false
     },
     {
       name: 'BRUSCHETTA',
       description: 'Grilled bread garlic, tomatoes, olive oil.',
-      price: 4.95
+      price: 4.95,
+      deleted: false
     }
   ];
   $scope.mains = [
